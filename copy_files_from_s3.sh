2c05be40701227683ee8401ee2594509ace70b97
Rebecca Batorsky <rebecca.batorsky@claritasgenomics.com>
2016-05-24 12:49:41 -0400

    First commit

---
+++ copy_files_from_s3.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+numofargs=$#
+if [ $numofargs -eq 1 ]; then
+        acc=$1
+        if [[ ${#acc} == 14 ]]; then
+		echo "Searching for files"
+        else
+                echo "Input should be full accession number with dashes, e.g. 18-CG-16-01234"
+								echo $1
+                exit 1
+        fi
+else
+        echo "Input should be full accession number with dashes, e.g. 18-CG-16-01234"
+        exit 1
+fi
+
+high_cov=$(aws s3 ls --recursive claritas-prod-workspace | grep $acc | grep highCoverageByRefSeqGene.txt$ | awk '{print $4}')
+low_cov=$(aws s3 ls --recursive claritas-prod-workspace | grep $acc | grep low_coverage_regions.amplicons.bed$ | awk '{print $4}')
+qc=$(aws s3 ls --recursive claritas-prod-workspace | grep $acc| grep VariantQCResult.txt$ | awk '{print $4}')
+
+aws s3 cp "s3://claritas-prod-workspace/$high_cov" "${acc}_highCoverageByRefSeqGene.txt"
+aws s3 cp "s3://claritas-prod-workspace/$low_cov" "${acc}_low_coverage_regions.amplicons.bed"
+aws s3 cp "s3://claritas-prod-workspace/$qc" "${acc}_VariantQCResult.txt"
